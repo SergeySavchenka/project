81c861b5a13691c6bffbe3433968b92a5e7041d1
SergeySavchenka <118177405+SergeySavchenka@users.noreply.github.com>
2023-12-04 21:27:29 +0300

    Update app_window.py

--- app_window.py
+++ app_window.py
@@ -10,12 +10,13 @@ class AppWindow(QWidget):
 
     def initUI(self):
         self.setWindowTitle("Authorization")
-        self.setGeometry(100, 100, 400, 200)
+        self.setGeometry(0, 0, 400, 200)
 
         self.username_label = QLabel("login:")
         self.username_input = QLineEdit()
         self.password_label = QLabel("password:")
         self.password_input = QLineEdit()
+        self.password_input.setEchoMode(QLineEdit.EchoMode.Password)
         self.login_button = QPushButton("sign_in")
         self.login_button.clicked.connect(self.login)
 
@@ -31,17 +32,26 @@ class AppWindow(QWidget):
         close_button.clicked.connect(sys.exit)
         self.setLayout(layout)
 
+        self.center()
+
+    def center(self):
+        screen_geometry = QApplication.primaryScreen().geometry()
+        window_geometry = self.frameGeometry()
+        window_geometry.moveCenter(screen_geometry.center())
+        self.move(window_geometry.topLeft())
+
     def login(self):
         username = self.username_input.text().strip()
         password = self.password_input.text().strip()
-        if username == 'admin' and password == 'admin':
-            self.to_database_window()
-        elif username.split()[0] == 'driver' and username.split()[1].isdigit() and username == password:
-            self.to_driver_window()
-        elif username.split()[0] == 'engineer' and password.split()[1] == 'engineer':
-            self.to_engineer_window()
-        else:
-            self.error_message('Не существующий логин или пароль')
+        try:
+            if username == 'admin' and password == 'admin':
+                self.to_database_window()
+            elif username.split()[0] == 'driver' and username.split()[1].isdigit() and username == password:
+                self.to_driver_window()
+            else:
+                self.error_message('Не существующий логин или пароль')
+        except:
+            print('Введены некорректные значения')
 
     def error_message(self, text):
         QMessageBox.information(
@@ -59,17 +69,10 @@ class AppWindow(QWidget):
         self.currentUser = DriverWindow(self.username_input.text().split()[1])
         self.currentUser.show()
 
-    def to_engineer_window(self):
-        if self.currentUser:
-            self.currentUser.close()
-        self.currentUser = EngineerWindow()
-        self.currentUser.show()
-
 
 if __name__ == "__main__":
     from ChooseWindow import AdminChooseWindow
     from driver_window import DriverWindow
-    from engineer_window import EngineerWindow
     app = QApplication(sys.argv)
 
     app_window = AppWindow()
